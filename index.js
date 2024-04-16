948153a9968148940d44547ddeef089bf01ab914
sujan787 <sujanmoi787@gmail.com>
2024-04-16 16:39:35 +0530

    added new changes

--- index.js
+++ index.js
@@ -64,28 +64,6 @@ const lipSyncMessage = async (message) => {
 
 app.post("/chat", async (req, res) => {
   const userMessage = req.body.message;
-  if (!userMessage) {
-    res.send({
-      messages: [
-        {
-          text: "Hey dear... How was your day?",
-          audio: await audioFileToBase64("audios/intro_0.wav"),
-          lipsync: await readJsonTranscript("audios/intro_0.json"),
-          facialExpression: "smile",
-          animation: "Talking_1",
-        },
-        {
-          text: "I missed you so much... Please don't go for so long!",
-          audio: await audioFileToBase64("audios/intro_1.wav"),
-          lipsync: await readJsonTranscript("audios/intro_1.json"),
-          facialExpression: "sad",
-          animation: "Crying",
-        },
-      ],
-    });
-    return;
-  }
-
   let messages = await getAnswerFromGemini(userMessage);
 
   for (let i = 0; i < messages.length; i++) {
